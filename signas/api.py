b2783b3766251293c85a6e3db03b1c962eab4634
TAICHI1129 <train.x956@gmail.com>
2026-04-06 21:54:59 +0900

    Update api.py

--- signas/api.py
+++ signas/api.py
@@ -1,13 +1,13 @@
+import io
+import sys
+from contextlib import redirect_stdout
 from signas import cli
 
-def generate_key(password: str):
-    return cli.generate_keys(password)
+def _capture_output(func, *args, **kwargs):
+    buf = io.StringIO()
+    with redirect_stdout(buf):
+        func(*args, **kwargs)
+    return buf.getvalue()
 
 def sign(file, name, github=None, password=None):
-    return cli.sign_file(file, name, github, password)
-
-def verify(file):
-    return cli.verify_file(file)
-
-def hash(file):
-    return cli.hash_file(file)
+    return _capture_output(cli.sign_file, file, name, github, password)
