b4957c270a66fa82b79be35996d74dd26e057310
TAICHI1129 <train.x956@gmail.com>
2026-04-06 21:56:56 +0900

    Update api.py

--- signas/api.py
+++ signas/api.py
@@ -1,13 +1,30 @@
 import io
-import sys
-from contextlib import redirect_stdout
+from contextlib import redirect_stdout, redirect_stderr
 from signas import cli
+
 
 def _capture_output(func, *args, **kwargs):
     buf = io.StringIO()
-    with redirect_stdout(buf):
-        func(*args, **kwargs)
+    with redirect_stdout(buf), redirect_stderr(buf):
+        try:
+            func(*args, **kwargs)
+        except SystemExit:
+            # argparse系がexitする対策
+            pass
     return buf.getvalue()
 
-def sign(file, name, github=None, password=None):
-    return _capture_output(cli.sign_file, file, name, github, password)
+
+def generate_key():
+    return _capture_output(cli.generate_keys)
+
+
+def sign(file, signer, github=None):
+    return _capture_output(cli.sign_file, file, signer, github)
+
+
+def verify(file):
+    return _capture_output(cli.verify_file, file)
+
+
+def hash(file):
+    return _capture_output(cli.hash_file, file)
